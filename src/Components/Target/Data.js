4b73c9a416aa8ded29232e7afab694247cb80272
Yunika-Bajracharya <076bct095.yunika@pcampus.edu.np>
2022-10-30 18:21:11 +0545

    target page done

---
+++ src/Components/Target/Data.js
@@ -0,0 +1,47 @@
+import pregnantLady from '../../assets/pregnantLady.png';
+import birth from '../../assets/newBorn.png';
+import vaccine from '../../assets/vaccine.png';
+import healthPost from '../../assets/healthPost.png';
+import death from '../../assets/death.png';
+import health from '../../assets/health.png';
+
+
+
+export const targetData = [
+    {
+        image: pregnantLady,
+        title: 'Active Pregnancies',
+        duration: 'All time',
+        number: 3,
+    },
+    {
+        image: birth,
+        title: 'Births',
+        duration: 'This month',
+        number: 2,
+    },    
+    {
+        image: healthPost,
+        title: 'Deliveries at Facility',
+        duration: 'All time',
+        number: '90%',
+    },
+    {
+        image: vaccine,
+        title: 'Children Vaccinated',
+        duration: 'Past 3 months',
+        number: 1,
+    },
+    {
+        image: health,
+        title: 'Family planning follow-up',
+        duration: 'This month',
+        number: 1,
+    },
+    {
+        image: death,
+        title: 'Deaths reported',
+        duration: 'This month',
+        number: 0,
+    },
+]
